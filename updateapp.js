f5df27c138c88baf1af7aceee0ccd8354162e621
Srisadana <153275623+Srisadana@users.noreply.github.com>
2024-06-02 18:03:51 +0530

    Update updateapp.js

--- updateapp.js
+++ updateapp.js
@@ -9,7 +9,7 @@ function App() {
     return (
         <Router>
             <div>
-                <h1>DataTradeHub</h1>
+                <h1>Dataxchange/h1>
                 <Switch>
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
